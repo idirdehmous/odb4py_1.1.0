66a25cc4841471762799d6f2f2dd9e69598e8502
idir Dehmous <idirdehmous@gmail.com>
2026-02-10 10:45:31 +0100

    c headers files

---
+++ include/exceptions.h
@@ -0,0 +1,17 @@
+#define PY_SSIZE_T_CLEAN
+#include  <stdio.h>
+#include  "Python.h"
+
+
+
+extern PyObject* odb4py_Error;
+extern PyObject* odb4py_Warning;
+extern PyObject* odb4py_InterfaceError;
+extern PyObject* odb4py_DatabaseError;
+extern PyObject* odb4py_InternalError;
+extern PyObject* odb4py_OperationalError;
+extern PyObject* odb4py_ProgrammingError;
+extern PyObject* odb4py_IntegrityError;
+extern PyObject* odb4py_DataError;
+extern PyObject* odb4py_NotSupportedError;
+
