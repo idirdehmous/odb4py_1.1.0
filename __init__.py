dda83b21d62ce36fd713f85b7e47576789d4ced0
idir Dehmous <idirdehmous@gmail.com>
2026-03-01 13:14:47 +0100

    Init ODB env by import any module from the package

--- __init__.py
+++ __init__.py
@@ -1,3 +1,14 @@
 # -*-coding: utf-8 -*-
+# odb4py
+# Copyright (C) 2026 Royal Meteorological Institute of Belgium (RMI)
+#
+# Licensed under the Apache License, Version 2.0
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
+
+# Ensure that the env is initialized whichever module is called !
+from .utils.setting_env import OdbEnv
+if not globals().get("_ODB_ENV_INITIALIZED", False):
+    _env = OdbEnv()
+    _env.InitEnv()
+    _ODB_ENV_INITIALIZED = True
