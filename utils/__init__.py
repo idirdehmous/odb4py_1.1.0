389276b669983925fd92a9a64cb354ef0fa9289a
idir Dehmous <idirdehmous@gmail.com>
2026-03-01 13:12:11 +0100

    no explicit env inititalization , it s initialized by import

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,11 +3,20 @@
 #
 # Licensed under the Apache License, Version 2.0
 
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
-from  .parser        import StringParser  
+from  .parser        import StringParser
 from  .odb_ob        import OdbObject
-from  .odb_glossary  import OdbLexic 
-from  .setting_env   import OdbEnv  
+from  .odb_glossary  import OdbLexic
+from  .setting_env   import OdbEnv
+
 
 __all__=[ "StringParser", "OdbObject" , "OdbLexic", "OdbEnv" ]
+
+
+# Init once
+if not globals().get("_ODB_ENV_INITIALIZED", False):
+    _env = OdbEnv()
+    _env.Init()
+    _ODB_ENV_INITIALIZED = True
+
